f20c593804dfced31c3e54edfdf0a345f62e56cf
Alexandre Lipp <alexandre.lipp@gmail.com>
2021-02-08 10:31:18 -0500

    del the menu file

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:chess/screens/chessScreen.dart';
-import 'package:chess/screens/menuScreen.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import './providers/myProvider.dart';
